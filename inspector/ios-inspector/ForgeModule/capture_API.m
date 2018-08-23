8fb6e5531e50d83440b9ae0db5f7c916c82ffb32
Antoine van Gelder <antoine@trigger.io>
2018-08-23 08:55:40 +0200

    * Fixed: iOS app store rejections caused by unused permission API's

--- inspector/ios-inspector/ForgeModule/capture_API.m
+++ inspector/ios-inspector/ForgeModule/capture_API.m
@@ -6,10 +6,15 @@
 //  Copyright (c) 2018 Trigger Corp. All rights reserved.
 //
 
-#import "capture_API.h"
-#import "capture_Delegate.h"
 #import <MobileCoreServices/UTCoreTypes.h>
 #import <ForgeCore/UIActionSheet+UIAlertInView.h>
+
+#import "JLCameraPermission.h"
+#import "JLMicrophonePermission.h"
+
+#import "capture_API.h"
+#import "capture_Delegate.h"
+
 
 @implementation capture_API
 
@@ -64,5 +69,62 @@ NSString *io_trigger_capture_dialog_cancel = @"Cancel";
     }
 }
 
+
++ (void)check:(ForgeTask*)task permission:(NSString *)permission {
+    JLPermissionsCore* jlpermission = [self resolvePermission:permission];
+    if (jlpermission == NULL) {
+        [task success:[NSNumber numberWithBool:NO]];
+        return;
+    }
+
+    JLAuthorizationStatus status = [jlpermission authorizationStatus];
+    [task success:[NSNumber numberWithBool:status == JLPermissionAuthorized]];
+}
+
+
++ (void)request:(ForgeTask*)task permission:(NSString *)permission {
+    JLPermissionsCore* jlpermission = [self resolvePermission:permission];
+    if (jlpermission == NULL) {
+        [task success:[NSNumber numberWithBool:NO]];
+        return;
+    }
+
+    if ([jlpermission authorizationStatus] == JLPermissionAuthorized) {
+        [task success:[NSNumber numberWithBool:YES]];
+        return;
+    }
+
+    NSDictionary* params = task.params;
+    NSString* rationale = [params objectForKey:@"rationale"];
+    if (rationale != nil) {
+        [jlpermission setRationale:rationale];
+    }
+
+    [jlpermission authorize:^(BOOL granted, NSError * _Nullable error) {
+        [jlpermission setRationale:nil]; // reset rationale
+        if (error) {
+            [ForgeLog d:[NSString stringWithFormat:@"permissions.check '%@' failed with error: %@", permission, error]];
+        }
+        [task success:[NSNumber numberWithBool:granted]];
+    }];
+}
+
+
++ (JLPermissionsCore*)resolvePermission:(NSString*)permission {
+    JLPermissionsCore* ret = NULL;
+    if ([permission isEqualToString:@""]) {
+        [ForgeLog d:[NSString stringWithFormat:@"Permission not supported on iOS:%@", permission]];
+
+    } else if ([permission isEqualToString:@"ios.permission.camera"]) {
+        ret = [JLCameraPermission sharedInstance];
+    } else if ([permission isEqualToString:@"ios.permission.microphone"]) {
+        ret = [JLMicrophonePermission sharedInstance];
+
+    } else {
+        [ForgeLog w:[NSString stringWithFormat:@"Requested unknown permission:%@", permission]];
+    }
+
+    return ret;
+}
 
 @end
