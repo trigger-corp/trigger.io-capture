3ca8380d1a3cd694a2530b79dc3a6c0d2df79821
Antoine van Gelder <antoine@trigger.io>
2020-08-13 09:11:49 +0200

    Replace deprecated usage of UIAlertView with UIAlertController

--- inspector/ios-inspector/ForgeModule/JLPermissions/JLCameraPermission.m
+++ inspector/ios-inspector/ForgeModule/JLPermissions/JLCameraPermission.m
@@ -13,48 +13,48 @@
 #import <ForgeCore/JLPermissionsCore+Internal.h>
 
 @implementation JLCameraPermission {
-  AuthorizationHandler _completion;
+    AuthorizationHandler _completion;
 }
 
 + (instancetype)sharedInstance {
-  static JLCameraPermission *_instance = nil;
-  static dispatch_once_t onceToken;
-
-  dispatch_once(&onceToken, ^{
-    _instance = [[JLCameraPermission alloc] init];
-  });
-
-  return _instance;
+    static JLCameraPermission *_instance = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        _instance = [[JLCameraPermission alloc] init];
+    });
+    
+    return _instance;
 }
 
 #pragma mark - Microphone
 
 - (JLAuthorizationStatus)authorizationStatus {
-  if ([AVCaptureDevice respondsToSelector:@selector(authorizationStatusForMediaType:)]) {
-    AVAuthorizationStatus permission =
+    if ([AVCaptureDevice respondsToSelector:@selector(authorizationStatusForMediaType:)]) {
+        AVAuthorizationStatus permission =
         [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
-
-    switch (permission) {
-      case AVAuthorizationStatusAuthorized:
+        
+        switch (permission) {
+            case AVAuthorizationStatusAuthorized:
+                return JLPermissionAuthorized;
+            case AVAuthorizationStatusDenied:
+            case AVAuthorizationStatusRestricted:
+                return JLPermissionDenied;
+            case AVAuthorizationStatusNotDetermined:
+                return JLPermissionNotDetermined;
+        }
+    } else {
+        // Prior to iOS 8 all apps were authorized.
         return JLPermissionAuthorized;
-      case AVAuthorizationStatusDenied:
-      case AVAuthorizationStatusRestricted:
-        return JLPermissionDenied;
-      case AVAuthorizationStatusNotDetermined:
-        return JLPermissionNotDetermined;
     }
-  } else {
-    // Prior to iOS 8 all apps were authorized.
-    return JLPermissionAuthorized;
-  }
 }
 
 - (void)authorize:(AuthorizationHandler)completion {
-  [self authorizeWithTitle:[self defaultTitle:@"Camera"]
-                   message:[self defaultMessage]
-               cancelTitle:[self defaultCancelTitle]
-                grantTitle:[self defaultGrantTitle]
-                completion:completion];
+    [self authorizeWithTitle:[self defaultTitle:@"Camera"]
+                     message:[self defaultMessage]
+                 cancelTitle:[self defaultCancelTitle]
+                  grantTitle:[self defaultGrantTitle]
+                  completion:completion];
 }
 
 - (void)authorizeWithTitle:(NSString *)messageTitle
@@ -62,68 +62,79 @@
                cancelTitle:(NSString *)cancelTitle
                 grantTitle:(NSString *)grantTitle
                 completion:(AuthorizationHandler)completion {
-  if ([AVCaptureDevice respondsToSelector:@selector(authorizationStatusForMediaType:)]) {
-    AVAuthorizationStatus permission =
+    if ([AVCaptureDevice respondsToSelector:@selector(authorizationStatusForMediaType:)]) {
+        AVAuthorizationStatus permission =
         [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
-    switch (permission) {
-      case AVAuthorizationStatusAuthorized: {
+        switch (permission) {
+            case AVAuthorizationStatusAuthorized: {
+                if (completion) {
+                    completion(true, nil);
+                }
+            } break;
+            case AVAuthorizationStatusDenied:
+            case AVAuthorizationStatusRestricted: {
+                if (completion) {
+                    completion(false, [self previouslyDeniedError]);
+                }
+            } break;
+            case AVAuthorizationStatusNotDetermined: {
+                _completion = completion;
+                if (self.isExtraAlertEnabled) {
+                    UIAlertController *alert =
+                        [UIAlertController alertControllerWithTitle:messageTitle
+                                                            message:message
+                                                     preferredStyle:UIAlertControllerStyleAlert];
+                        [alert addAction:[UIAlertAction actionWithTitle:cancelTitle
+                                                                  style:UIAlertActionStyleCancel
+                                                                handler:^(UIAlertAction * _Nonnull action) {
+                            if (self->_completion) {
+                                self->_completion(false, nil);
+                            }
+                        }]];
+                        [alert addAction:[UIAlertAction actionWithTitle:grantTitle
+                                                                  style:UIAlertActionStyleDefault
+                                                                handler:^(UIAlertAction * _Nonnull action) {
+                            [self actuallyAuthorize];
+                        }]];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [[[ForgeApp sharedApp] viewController] presentViewController:alert animated:YES completion:nil];
+                    });
+                } else {
+                    [self actuallyAuthorize];
+                }
+            } break;
+        }
+    } else {
+        // Prior to iOS 8 all apps were authorized.
         if (completion) {
-          completion(true, nil);
+            completion(true, nil);
         }
-      } break;
-      case AVAuthorizationStatusDenied:
-      case AVAuthorizationStatusRestricted: {
-        if (completion) {
-          completion(false, [self previouslyDeniedError]);
-        }
-      } break;
-      case AVAuthorizationStatusNotDetermined: {
-        _completion = completion;
-        if (self.isExtraAlertEnabled) {
-          UIAlertView *alert = [[UIAlertView alloc] initWithTitle:messageTitle
-                                                          message:message
-                                                         delegate:self
-                                                cancelButtonTitle:cancelTitle
-                                                otherButtonTitles:grantTitle, nil];
-          dispatch_async(dispatch_get_main_queue(), ^{
-            [alert show];
-          });
-        } else {
-          [self actuallyAuthorize];
-        }
-      } break;
     }
-  } else {
-    // Prior to iOS 8 all apps were authorized.
-    if (completion) {
-      completion(true, nil);
-    }
-  }
 }
 
 - (JLPermissionType)permissionType {
-  return JLPermissionCamera;
+    return JLPermissionCamera;
 }
 
 - (void)actuallyAuthorize {
-  [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo
-                           completionHandler:^(BOOL granted) {
-                             if (_completion) {
-                               dispatch_async(dispatch_get_main_queue(), ^{
-                                 if (granted) {
-                                   _completion(true, nil);
-                                 } else {
-                                   _completion(false, nil);
-                                 }
-                               });
-                             }
-                           }];
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo
+                             completionHandler:^(BOOL granted) {
+        if (self->_completion) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (granted) {
+                    self->_completion(true, nil);
+                } else {
+                    self->_completion(false, nil);
+                }
+            });
+        }
+    }];
 }
 
 - (void)canceledAuthorization:(NSError *)error {
-  if (_completion) {
-    _completion(false, error);
-  }
+    if (_completion) {
+        _completion(false, error);
+    }
 }
 
 @end
--- inspector/ios-inspector/ForgeModule/JLPermissions/JLMicrophonePermission.m
+++ inspector/ios-inspector/ForgeModule/JLPermissions/JLMicrophonePermission.m
@@ -14,59 +14,59 @@
 #define kJLAskedForMicrophonePermission @"JLAskedForMicrophonePermission"
 
 @implementation JLMicrophonePermission {
-  AuthorizationHandler _completion;
+    AuthorizationHandler _completion;
 }
 
 + (instancetype)sharedInstance {
-  static JLMicrophonePermission *_instance = nil;
-  static dispatch_once_t onceToken;
-
-  dispatch_once(&onceToken, ^{
-    _instance = [[JLMicrophonePermission alloc] init];
-  });
-
-  return _instance;
+    static JLMicrophonePermission *_instance = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        _instance = [[JLMicrophonePermission alloc] init];
+    });
+    
+    return _instance;
 }
 
 #pragma mark - Microphone
 
 - (JLAuthorizationStatus)authorizationStatus {
-  AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-  if ([audioSession respondsToSelector:@selector(recordPermission)]) {
-    AVAudioSessionRecordPermission permission = [[AVAudioSession sharedInstance] recordPermission];
-    switch (permission) {
-      case AVAudioSessionRecordPermissionGranted:
-        return JLPermissionAuthorized;
-      case AVAudioSessionRecordPermissionDenied:
-        return JLPermissionDenied;
-      case AVAudioSessionRecordPermissionUndetermined:
-        return JLPermissionNotDetermined;
-    }
-  } else {
-    bool previouslyAsked =
-        [[NSUserDefaults standardUserDefaults] boolForKey:kJLAskedForMicrophonePermission];
-    if (previouslyAsked) {
-      dispatch_semaphore_t sema = dispatch_semaphore_create(0);
-      __block BOOL hasAccess;
-      [audioSession requestRecordPermission:^(BOOL granted) {
-        hasAccess = granted;
-        dispatch_semaphore_signal(sema);
-      }];
-      dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
-
-      return (hasAccess) ? JLPermissionAuthorized : JLPermissionDenied;
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    if ([audioSession respondsToSelector:@selector(recordPermission)]) {
+        AVAudioSessionRecordPermission permission = [[AVAudioSession sharedInstance] recordPermission];
+        switch (permission) {
+            case AVAudioSessionRecordPermissionGranted:
+                return JLPermissionAuthorized;
+            case AVAudioSessionRecordPermissionDenied:
+                return JLPermissionDenied;
+            case AVAudioSessionRecordPermissionUndetermined:
+                return JLPermissionNotDetermined;
+        }
     } else {
-      return JLPermissionNotDetermined;
+        bool previouslyAsked =
+        [[NSUserDefaults standardUserDefaults] boolForKey:kJLAskedForMicrophonePermission];
+        if (previouslyAsked) {
+            dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+            __block BOOL hasAccess;
+            [audioSession requestRecordPermission:^(BOOL granted) {
+                hasAccess = granted;
+                dispatch_semaphore_signal(sema);
+            }];
+            dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+            
+            return (hasAccess) ? JLPermissionAuthorized : JLPermissionDenied;
+        } else {
+            return JLPermissionNotDetermined;
+        }
     }
-  }
 }
 
 - (void)authorize:(AuthorizationHandler)completion {
-  [self authorizeWithTitle:[self defaultTitle:@"Microphone"]
-                   message:[self defaultMessage]
-               cancelTitle:[self defaultCancelTitle]
-                grantTitle:[self defaultGrantTitle]
-                completion:completion];
+    [self authorizeWithTitle:[self defaultTitle:@"Microphone"]
+                     message:[self defaultMessage]
+                 cancelTitle:[self defaultCancelTitle]
+                  grantTitle:[self defaultGrantTitle]
+                  completion:completion];
 }
 
 - (void)authorizeWithTitle:(NSString *)messageTitle
@@ -74,78 +74,89 @@
                cancelTitle:(NSString *)cancelTitle
                 grantTitle:(NSString *)grantTitle
                 completion:(AuthorizationHandler)completion {
-  AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-  if ([audioSession respondsToSelector:@selector(recordPermission)]) {
-    AVAudioSessionRecordPermission permission = [audioSession recordPermission];
-    switch (permission) {
-      case AVAudioSessionRecordPermissionGranted: {
-        if (completion) {
-          completion(true, nil);
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    if ([audioSession respondsToSelector:@selector(recordPermission)]) {
+        AVAudioSessionRecordPermission permission = [audioSession recordPermission];
+        switch (permission) {
+            case AVAudioSessionRecordPermissionGranted: {
+                if (completion) {
+                    completion(true, nil);
+                }
+            } break;
+            case AVAudioSessionRecordPermissionDenied: {
+                if (completion) {
+                    completion(false, [self previouslyDeniedError]);
+                }
+            } break;
+            case AVAudioSessionRecordPermissionUndetermined: {
+                _completion = completion;
+                if (self.isExtraAlertEnabled) {
+                    UIAlertController *alert =
+                        [UIAlertController alertControllerWithTitle:messageTitle
+                                                            message:message
+                                                     preferredStyle:UIAlertControllerStyleAlert];
+                        [alert addAction:[UIAlertAction actionWithTitle:cancelTitle
+                                                                  style:UIAlertActionStyleCancel
+                                                                handler:^(UIAlertAction * _Nonnull action) {
+                            if (self->_completion) {
+                                self->_completion(false, nil);
+                            }
+                        }]];
+                        [alert addAction:[UIAlertAction actionWithTitle:grantTitle
+                                                                  style:UIAlertActionStyleDefault
+                                                                handler:^(UIAlertAction * _Nonnull action) {
+                            [self actuallyAuthorize];
+                        }]];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [[[ForgeApp sharedApp] viewController] presentViewController:alert animated:YES completion:nil];
+                    });
+                } else {
+                    [self actuallyAuthorize];
+                }
+            } break;
         }
-      } break;
-      case AVAudioSessionRecordPermissionDenied: {
-        if (completion) {
-          completion(false, [self previouslyDeniedError]);
-        }
-      } break;
-      case AVAudioSessionRecordPermissionUndetermined: {
-        _completion = completion;
-        if (self.isExtraAlertEnabled) {
-          UIAlertView *alert = [[UIAlertView alloc] initWithTitle:messageTitle
-                                                          message:message
-                                                         delegate:self
-                                                cancelButtonTitle:cancelTitle
-                                                otherButtonTitles:grantTitle, nil];
-          dispatch_async(dispatch_get_main_queue(), ^{
-            [alert show];
-          });
-        } else {
-          [self actuallyAuthorize];
-        }
-      } break;
+    } else {
+        [audioSession requestRecordPermission:^(BOOL granted) {
+            [[NSUserDefaults standardUserDefaults] setBool:true forKey:kJLAskedForMicrophonePermission];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+            if (completion) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (granted) {
+                        completion(granted, nil);
+                    } else {
+                        completion(false, nil);
+                    }
+                });
+            }
+        }];
     }
-  } else {
-    [audioSession requestRecordPermission:^(BOOL granted) {
-      [[NSUserDefaults standardUserDefaults] setBool:true forKey:kJLAskedForMicrophonePermission];
-      [[NSUserDefaults standardUserDefaults] synchronize];
-      if (completion) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-          if (granted) {
-            completion(granted, nil);
-          } else {
-            completion(false, nil);
-          }
-        });
-      }
-    }];
-  }
 }
 
 - (JLPermissionType)permissionType {
-  return JLPermissionMicrophone;
+    return JLPermissionMicrophone;
 }
 
 - (void)actuallyAuthorize {
-  AVAudioSession *session = [[AVAudioSession alloc] init];
-  NSError *error;
-  [session setCategory:@"AVAudioSessionCategoryPlayAndRecord" error:&error];
-  [session requestRecordPermission:^(BOOL granted) {
-    if (_completion) {
-      dispatch_async(dispatch_get_main_queue(), ^{
-        if (granted) {
-          _completion(true, nil);
-        } else {
-          _completion(false, nil);
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    NSError *error;
+    [session setCategory:@"AVAudioSessionCategoryPlayAndRecord" error:&error];
+    [session requestRecordPermission:^(BOOL granted) {
+        if (self->_completion) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (granted) {
+                    self->_completion(true, nil);
+                } else {
+                    self->_completion(false, nil);
+                }
+            });
         }
-      });
-    }
-  }];
+    }];
 }
 
 - (void)canceledAuthorization:(NSError *)error {
-  if (_completion) {
-    _completion(false, error);
-  }
+    if (_completion) {
+        _completion(false, error);
+    }
 }
 
 @end
